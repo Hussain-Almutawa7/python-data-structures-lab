0234e913610536e1acb91c171af876bde8d3f339
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-17 10:53:39 +0300

    some fixes to match the requirments

--- exercises.py
+++ exercises.py
@@ -41,10 +41,10 @@ print('Exercise 1:', manage_students())
 def combine_foods():
     # your code here
     foods = ("Pizza", "Chicken", "Burger")
-    meal = []
+    meal = ""
     
     for food in foods:
-        meal.append(food)
+        meal += food
     
     return meal
 # Call the function and print the result
@@ -104,5 +104,17 @@ def list_home_town_items():
     return home_town_items
 # Call the function and print the result
 print('Exercise 5:', list_home_town_items())
+
+
+# Exercise 6: Celebrate Students
+#
+# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
+# For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
+
+def create_awesome_students():
+    # your code here
+    
+# Call the function and print the result
+print('Exercise 6:', create_awesome_students())
 
 
